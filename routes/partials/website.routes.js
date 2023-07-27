60c9793a263823c5a5dc4b3c2555f47cac4f6aa1
Md-shefat-masum <mshefat924@gmail.com>
2023-07-27 12:59:24 +0600

    create web page routes and controller function

--- routes/partials/website.routes.js
+++ routes/partials/website.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const isAuthMiddleware = require("../../app/middlewares/isAuth.middleware");
 const { share_check_auth } = require("../..");
+const website_controller = require("../../app/controllers/frontend/wbsite.controller");
 const router = express.Router();
 
 router
-	.get("/", function (req, res) {
-		return res.render("home");
-	})
+	.get("/", website_controller.home)
+	.get("/category/:category_name/:category_id", website_controller.category_post)
 	.get("/about", function (req, res) {
 		return res.render("about");
 	})
