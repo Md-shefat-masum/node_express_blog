e1bfc9f5d416382da676b92014b4e0e84acf7b3a
Md-shefat-masum <mshefat924@gmail.com>
2023-09-23 22:39:09 +0600

    share categories and auth templates setup

--- app/controllers/frontend/wbsite.controller.js
+++ app/controllers/frontend/wbsite.controller.js
@@ -1,17 +1,17 @@
 const blogModel = require("../../models/blog.model");
 const categoryModel = require("../../models/category.model");
 
+const get_categories = async () => await categoryModel.find();
 const controllers = {
 	folder_prefix: ``,
 	route_prefix: ``,
 
 	home: async function (req, res) {
 		let blogs = await blogModel.find().populate('creator').populate('category');
-        let categories = await categoryModel.find();
-
+        
 		return res.render(`home`, {
 			blogs,
-            categories,
+            categories: await get_categories(),
 		});
 	},
 	category_post: async function (req, res) {
@@ -23,6 +23,16 @@ const controllers = {
 		return res.render(`category_post`, {
             categories,
             blogs
+		});
+	},
+	login: async function(req,res){
+		return res.render("auth/login",{
+			categories: await get_categories(),
+		});
+	},
+	signup: async function(req,res){
+		return res.render("auth/register",{
+			categories: await get_categories(),
 		});
 	},
 };
