9ed7dbaf6d971acdd0fe1d52c265d8e78de10659
Md-shefat-masum <mshefat924@gmail.com>
2023-10-15 20:47:44 +0600

    new seed update

--- db.seed.js
+++ db.seed.js
@@ -3,13 +3,19 @@ const blogSeeder = require("./app/models/seeders/blog.seeder");
 const categorySeeder = require("./app/models/seeders/category.seeder");
 const translatorSeeder = require("./app/models/seeders/translator.seeder");
 const writerSeeder = require("./app/models/seeders/writer.seeder");
+const { db_url } = require("./configs/db.config");
+const { default: mongoose } = require("mongoose");
 
-const seed = async () =>{
-    await userSeeder();
-    await categorySeeder();
-    await writerSeeder();
-    await translatorSeeder();
-    await blogSeeder();
+const seed = async () => {
+    mongoose.connect(db_url)
+        .then(async () => {
+            await userSeeder();
+            await categorySeeder();
+            await writerSeeder();
+            await translatorSeeder();
+            await blogSeeder();
+            console.log('\n seed end');
+        })
 }
 
 seed();
