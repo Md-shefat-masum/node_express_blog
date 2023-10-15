cb8b0427a3ef957f12350d62b2d06f0a4b6f8fb0
Md-shefat-masum <mshefat924@gmail.com>
2023-10-15 20:48:12 +0600

    category seed update

--- app/models/seeders/category.seeder.js
+++ app/models/seeders/category.seeder.js
@@ -2,7 +2,7 @@ const { default: mongoose, Schema } = require("mongoose");
 const categoryModel = require("../category.model");
 const { db_url } = require("../../../configs/db.config");
 
-let categories = [
+let data = [
 	{
 		title: "Opinion",
 		creator: "645fc7c1d7c51530643212e0",
@@ -21,30 +21,9 @@ let categories = [
 	},
 ];
 
-module.exports = () =>
-	mongoose.connect(db_url).then(async () => {
-		console.log("\n");
-		console.log("catetgory seeding");
-
-		await categoryModel.deleteMany({});
-		// let response = await categoryModel.insertMany(categories);
-
-		// for (let index = 0; index < 50; index++) {
-		// 	await categoryModel.create({
-		// 		title: "Category " + (index + 1),
-		// 		creator: "645fc48cef1e9e5ad5a12089",
-		// 	});
-		// }
-
-		console.log("catetgory seeding complete");
-		categories.forEach(async (item) => {
-			let category = new categoryModel(item);
-			await category.save();
-			console.log(category);
-		});
-
-		console.log("\n");
-
-		// let cate = await categoryModel.findOne({}).populate('creator')
-		// console.log(cate);
-	});
+module.exports = async () => {
+	console.log("\n");
+	console.log("user seeding");
+	await categoryModel.deleteMany({});
+	let response = await categoryModel.insertMany(data);
+}
