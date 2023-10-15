e7a5af6a0a5b82a6a74308064dc746fdb571652c
Md-shefat-masum <mshefat924@gmail.com>
2023-10-15 20:48:39 +0600

    db config

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
-const db_url = "mongodb://127.0.0.1:27017/blogDB";
+// const db_url = "mongodb://127.0.0.1:27017/blogDB";
+const db_url = "mongodb://root:m2451238228@103.191.241.251:27017/blogDB?authSource=admin";
 
 module.exports = {
     db_url,
